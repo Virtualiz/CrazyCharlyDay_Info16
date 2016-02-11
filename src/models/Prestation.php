c34d13989f9f56b5ef0c16f69b225999e1fd4b4e
sinteff3u <bruno.sinteff@etu.univ-lorraine.fr>
2016-02-11 10:56:19 +0100

    Ajout des modèles Prestation et Type, à tester : les liaisons etc ...

---
+++ src/models/Prestation.php
@@ -0,0 +1,19 @@
+<?php
+namespace src\models;
+
+use Illuminate\Database\Eloquent\Model;
+/**
+ * Created by PhpStorm.
+ * User: Bruno
+ * Date: 11/02/2016
+ * Time: 10:44
+ */
+class Prestation extends Model
+{
+    protected $table = "prestation";
+    protected $timestamps = false;
+
+    function type(){
+        return $this->belongsTo("type");
+    }
+}
---
+++ src/models/Type.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bruno
+ * Date: 11/02/2016
+ * Time: 10:54
+ */
+
+namespace src\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Type extends Model
+{
+    protected $table = "type";
+    protected $timestamps = false;
+
+    function prestations(){
+        return $this->hasMany("prestation");
+    }
+}
