9671eb06fc68095ae41195345effbe957f317960
Allan <Koutai.Game@gmail.com>
2016-02-11 21:40:19 +0100

    modif affichage catalogue

--- src/View/Catalogue.php
+++ src/View/Catalogue.php
@@ -1,6 +1,10 @@
 <?php
 
-namespace View;
+namespace view;
+
+
+use View\BasicView;
+
 
 class Catalogue extends BasicView{
 	
@@ -18,58 +22,28 @@ class Catalogue extends BasicView{
 		$root = \Slim\Slim::getInstance()->request->getRootUri();
 		$app = \Slim\Slim::getInstance();
 
-		$scrib = '';
+		$scrib = '<link href="../css/bootstrap.min.css" rel="stylesheet"><link href="../css/starter-template.css" rel="stylesheet"><div class="container"><div class="starter-template">';
 
-		$scrib='
-				<section>
-				
-					<h1>Attention</h1>
-				
-						<form><fieldset>';
-		
-		foreach ($this->tab as  $val){
-			if($val["type"]==1){
-				$scrib.='		<input type="checkbox" id="fjf1" value="'.$val["id"].'">'.$val["nom"].'</input>';		
-			}
-		}
-					$scrib.='
-						</fieldset></form>
-					<h1>Activité</h1>
-						<form><fieldset>';
-					
-		foreach ( $this->tab as $val ) {
-			if ($val ["type"] == 2) {
-				$scrib .= '		<input type="checkbox" id="fjf1" value="'.$val["id"].'">' . $val ["nom"] . '</input>';
-			}
-		}
-					
-		
-					$scrib.='
-						</fieldset></form>
-					<h1>Restauration</h1>
-						<form><fieldset>';
-					
-		foreach ( $this->tab as $val ) {
-			if ($val ["type"] == 3) {
-				$scrib .= '		<input type="checkbox" id="fjf1" value="'.$val["id"].'">' . $val ["nom"] . '</input>';
-			}
-		}
-					
-					$scrib.='
-						</fieldset></form>
-					<h1>Hébergement</h1>
-						<form><fieldset>';
-					
-		foreach ( $this->tab as $val ) {
-			if ($val ["type"] == 4) {
-				$scrib .= '		<input type="checkbox" id="fjf1" value="'.$val["id"].'">' . $val ["nom"] . '</input>';
-			}
-		}
-				
-				$scrib.='</fieldset></form></section>';
+		if ($indice == '1') {
 
-			echo $scrib;
-		
+
+			foreach ($this->tab as $prestation) {
+				$scrib .= "
+			<section class='row'>
+			<div class='col-lg-12 thumbnail'>
+			<div><h1>" . $prestation->nom . "</h1></div>
+				<div class='col-lg-6'>
+					<div class='col-lg-12'><p>" . $prestation->descr . "</p></div>
+					<div class='col-lg-12'><p> Prix : " . $prestation->prix . " €</p></div>
+					<div class='col-lg-12'><a class='btn btn-primary' href='#'>Ajouter à la pochette</a></div>
+				</div>
+				<div class='col-lg-6'><img src='../doc/img/" . $prestation->img . "' height='300'></div></div>
+			</section>
+			";
+			}
+		}
+		$scrib .= '</div></div>';
+
+		echo $scrib;
 	}
-	
 }
